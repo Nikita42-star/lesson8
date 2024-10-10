ab81164c4fc6c15e5e46e0cc0d68556d2feb62a3
Никита <n.s.galeev@gmail.com>
2024-10-10 15:26:29 +0300

    Add files via upload

---
+++ module_2_1.py
@@ -0,0 +1,10 @@
+from ftplib import parse150
+
+first = input('Введите первое число: ')
+second = input('Введите второе число:  ')
+third = input('Введите третье число:   ')
+if first == second == third:
+    print(3)
+elif first != second != third != first:
+    print(0)
+else: print(2)
